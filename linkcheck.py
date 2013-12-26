d88421cda33d9201ee5e459c817cdb4718facf0c
instantaphex <johnmorganbryan@gmail.com>
2013-12-26 12:30:59 -0800

    Combined classes into linkcheck.py

--- linkcheck.py
+++ linkcheck.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 from queue import Queue
-from worker import worker
-from backlink import backlink
 import csv
 import sys
 import re
 import argparse
+import urllib.error
+import urllib.parse
+import urllib.request
+import http.client
+from time import sleep
 import tldextract as tld
+from threading import Thread
+from bs4 import BeautifulSoup as bs
 
 parser = argparse.ArgumentParser(description='Check list of URLs for existence of link in html')
 parser.add_argument('-d','--domain', help='The domain you would like to search for a link to', required=True)
@@ -28,6 +33,70 @@ PURPLE = '\033[95m'
 ORANGE = '\033[91m'
 BOLD = '\033[1m'
 ENDC = '\033[0m'
+
+class backlink:
+	def __init__(self, url, index, domain):
+		self.url = url
+		self.status = 'UNKNOWN' 
+		self.index = index
+		self.domain = domain
+    
+class worker(Thread):
+	def __init__(self, input_queue, output_queue, domain):
+		super(worker, self).__init__()
+		self.daemon = True
+		self.cancelled = False
+		self.input_queue = input_queue
+		self.output_queue = output_queue
+		self.domain = domain
+
+	def url_sanitize(self, url):
+		parsed = urllib.parse.urlparse(url)
+		return urllib.parse.urlunparse(urllib.parse.quote(x) for x in parsed)
+
+	def check_url(self, url):
+		req = urllib.request.Request(url)
+		req.add_header('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36')
+		try:
+			html = urllib.request.urlopen(req).read()
+		except urllib.error.HTTPError as e:
+			html = e.read()
+		soup = bs(html)
+		link = soup.find_all('a', attrs={'href': re.compile(self.domain)})
+		if len(link) > 0: #link from domain was found
+			return 'EXISTS'
+		else:
+			return 'REMOVED'
+
+	#Workers
+	def run(self):
+		while not self.cancelled:
+			link = self.input_queue.get()
+			if link is None:
+				self.input_queue.task_done()
+				self.input_queue.put(None)
+				break
+			try:
+				link.status = self.check_url(link.url)
+			except urllib.error.HTTPError as e:
+				link.status = str(e)
+			except urllib.error.URLError as e:
+				link.status = str(e)
+			except http.client.BadStatusLine as e:
+				link.status = str(e)
+			except UnicodeDecodeError:
+				self.check_url(self.sanitize_url(link.url))
+			except ConnectionResetError:
+				input_queue.put(link)
+				break
+			self.output_queue.put(link)
+			self.input_queue.task_done()
+			sleep(0.01)
+
+	def cancel(self):
+		self.cancelled = True
+
+
 
 #Queues
 input_queue = Queue()
@@ -71,7 +140,7 @@ with open(OUTFILE, 'a') as f:
 	for i in links:
 		c.writerow((i.index, i.url, i.status))
 
-if ARGS['create-disavow']:
+if CREATE_DISAVOW:
 	#remove duplicates from disavow_links list
 	disavow_links = set(disavow_links)
 
